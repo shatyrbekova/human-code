a2a5ecea4b6b4486d7abd62eb5a48a2fa6f60f84
shatyrbekova <perizat.shatyrbekova@gmail.com>
2021-06-23 17:06:18 +0200

    Human code

---
+++ script.js
@@ -0,0 +1,52 @@
+
+
+    //!  3. Gli amici ti hanno appena invitato a passare con loro il sabato a mare, unirsi alla
+    //!  comitiva.
+
+/** 
+ * 3.1) Prima di dargli  una risposta positiva, controllo se sabato ho un altro impegno;
+ *     3.1.1) Se avessi un impegno, dovrei rifiutare l'invito;
+ *     3.1.2) Altrimenti, ci vado. //?Però prima di rispondere chiedo alcune cose che mi interessano.
+ * 
+ * //* Prendo il cellulare e chiamo ad uno degli amici.
+ * 
+ * 3.2) Chiedo le ulteriori informazioni. In particolare:
+ *     3.2.1) Chi ci sarà? 
+ *         3.2.1.1) Se ci sono le persone che sono antipatiche non ci vado;
+ *         3.2.1.2) Se  sono le persone simpatiche ci vado volentieri. 
+ *     3.2.2) Se qc di loro che ha avuto un contatto fisico diretto con un caso confermato COVID-19 ossia
+ *            ha viaggiato ai Paesi con il rischio elevato di contagio; 
+ *      
+  
+ *     3.2.3) Come arrivare al mare? 
+ *         3.2.3.1) Raggiungo il mare insieme a loro;
+ *         3.3.3.2) Prendo i mezzi di trasporto pubblico;
+ * 
+// * Per le azioni da 3.3 a 3.5 mi collego a Google per trovare le risposte.
+ * 
+ * 3.3) Quali mezzi devo prendere per arrivare al mare?
+ * 
+ * 3.4) Che tempo fa sabato?                
+ * 
+ * 3.5) Acquisto online i biglietti di andata e ritorno;
+ * 
+ *  //*Preparo la borsa per la spiaggia, mi alzo dalla sedia.
+ * 
+ * 3.6) Mi avvicino all'armadio ed apro la porta dell'armadio;
+ *      3.6.1) Prendo la borsa per la spiaggia dall'armadio;
+ *      3.6.2) Ci metto delle che mi potrebbero servire al mare.( la crema solare, il telo, l'asciugamano ed ecc..);
+ *      3.6.3) Se non ho la crema, vado in farmacia per comprarla. (in farmacia compro anche la crema dopo il sole e l'olio d'oliva per abbronzarsi ) //?borsaPerLaSpiaggia.push
+ * 
+
+ * 3.7) Cosa si mangia? //* Penso ad una soluzione.
+ *     3.7.1) Porto qualche snack; //*Vado in cucina, apro l'armadio in cui ci sono delle buste, ne prendo una;
+ *        3.7.1.1)  Apro la porta del frigorifero. Se ci sono gli ingredienti adatti per il mare, 
+ *                 li prendo e li metto nella  busta. 
+ * 3.8) Devo avvisare i miei che vado al mare. 
+ *    3.8.1) Ritorno di nuovo alla mia stanza, prendo il cellulare e chiamo. 
+ * 
+ * 3.9) Chiama il mio amico e mi chiede di portare l'ombrellone;
+ *    3.9.1) Se non c'è l'ho, dico che non c'è l'ho e lo prendiamo al mare;
+ *    3.9.1) Se c'è l'ho, lo prendo. 
+ */
+ 
